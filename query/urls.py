fab972283a0480ee39d05caf28bce8391dffe572
lisongwei1593 <lisongwei1593@gmail.com>
2016-03-22 11:30:28 +0800

    UPD:提交detail页面

--- query/urls.py
+++ query/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('query.views',
     url(r'^$', 'index', name='index'),
+    url(r'^detail/(?P<bid>\d+)/$', 'detail', name='detail'),
 )
--- query/views.py
+++ query/views.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-s
 from django.shortcuts import render
 
-from query.models import BusinessMan
+from query.models import BusinessMan, Business
 
 
 # Create your views here.
 def index(request):
     business_men = BusinessMan.objects.all()
     return render(request, 'index.html', locals())
+
+
+def detail(request,bid):
+    business_man = BusinessMan.objects.get(id=bid)
+    business = Business.objects.filter(business_man=business_man).order_by('business_time')
+    return render(request, 'business_detail.html', locals())
